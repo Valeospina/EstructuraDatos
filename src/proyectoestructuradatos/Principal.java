9c8b03abdcd52c41fc10220799555d169792e890
Miguelon <miguel.otarola03@gmail.com>
2024-03-12 15:51:22 -0600

    Clase prin editada

--- src/proyectoestructuradatos/Principal.java
+++ src/proyectoestructuradatos/Principal.java
@@ -15,6 +15,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         System.out.println("hola");
+         System.out.println("holaaaaaaaa");
         
         
     }

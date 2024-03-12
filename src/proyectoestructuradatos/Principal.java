d836114ca83014acc3281c14c2585a85dd6c0d27
Valeospina <rengifovale7@gmail.com>
2024-03-12 15:47:54 -0600

    agregar clase

--- src/proyectoestructuradatos/Principal.java
+++ src/proyectoestructuradatos/Principal.java
@@ -8,13 +8,15 @@ package proyectoestructuradatos;
  *
  * @author TALLER
  */
-public class ProyectoEstructuraDatos {
+public class Principal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("hola");
+        
+        
     }
     
 }

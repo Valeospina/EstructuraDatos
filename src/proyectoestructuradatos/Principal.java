96d2ef2f7411545b627a0202688844712b828d37
Valeospina <rengifovale7@gmail.com>
2024-03-14 08:50:36 -0600

    borre clase

--- src/proyectoestructuradatos/Principal.java
+++ src/proyectoestructuradatos/Principal.java
@@ -15,7 +15,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         System.out.println("hola");
-         System.out.println("holaaaaaaaa");
+        System.out.println("holaaaaaaaa");
         
         
     }

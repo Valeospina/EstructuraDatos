abd1bc10a6b7a5c64727c4464cc1c3638117a16b
Valeospina <rengifovale7@gmail.com>
2024-04-02 17:08:00 -0600

    cambio en clase principal

--- src/proyectoestructuradatos/Principal.java
+++ src/proyectoestructuradatos/Principal.java
@@ -16,6 +16,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("holaaaaaaaa");
+        System.out.println("Valee");
         
         
     }
